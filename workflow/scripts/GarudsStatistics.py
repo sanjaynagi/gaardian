0fdb54c73236768120b43264ad00587c2129ef30
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2021-12-23 17:58:27 +0000

    lots of edits, snakemake workflow infrastructure built, running now.

---
+++ workflow/scripts/GarudsStatistics.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+"""
+
+import sys
+sys.stderr = open(snakemake.log[0], "w")
+
+from tools import loadZarrArrays, getCohorts, saveAndPlot, log
+from pathlib import Path
+import pandas as pd
+import allel
+import dask.array as da
+import scipy
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# Garuds Selection Scans # 
+chrom = snakemake.wildcards['chrom']
+stat = snakemake.params['GarudsStat']
+windowSize = snakemake.params['windowSize']
+windowStep = snakemake.params['windowStep']
+cutHeight = snakemake.params['cutHeight'] if stat in ['G12', 'G123'] else []
+genotypePath = snakemake.input['genotypes'] if stat in ['G12', 'G123'] else []
+haplotypePath = snakemake.input['haplotypes'] if stat in ['H1', 'H12', 'H2/1'] else []
+positionsPath = snakemake.input['positions']
+siteFilterPath = snakemake.input['siteFilters']
+
+# Read metadata 
+metadata = pd.read_csv(snakemake.params['metadata'], sep=",")
+metadata['location'] = metadata['location'].str.split(".").str.get(0)
+
+# Load arrays 
+if stat in ['H1', 'H12', 'H2/1']:
+    haps, pos = loadZarrArrays(haplotypePath, positionsPath, siteFilterPath=siteFilterPath, haplotypes=True)
+elif stat in ['G12', 'G123']:
+    snps, pos = loadZarrArrays(genotypePath, positionsPath, siteFilterPath=siteFilterPath, haplotypes=False)
+else:
+    raise AssertionError("The statistic selected is not 'G12, G123, or H12")
+
+
+
+# Define functions
+def clusterMultiLocusGenotypes(gnalt, cut_height=0.1, metric='euclidean', g=2):
+    """
+    Hierarchically clusters genotypes and calculates G12 statistic. 
+    """
+    # cluster the genotypes in the window
+    dist = scipy.spatial.distance.pdist(gnalt.T, metric=metric)
+    if metric in {'hamming', 'jaccard'}:
+        # convert distance to number of SNPs, easier to interpret
+        dist *= gnalt.shape[0]
+
+    Z = scipy.cluster.hierarchy.linkage(dist, method='single')
+    cut = scipy.cluster.hierarchy.cut_tree(Z, height=cut_height)[:, 0]
+    cluster_sizes = np.bincount(cut)
+    #clusters = [np.nonzero(cut == i)[0] for i in range(cut.max() + 1)] #returns indices of individuals in each cluster
+    
+    # get freq of clusters and sort by largest freq
+    freqs = cluster_sizes/gnalt.shape[1]
+    freqs = np.sort(freqs)[::-1]
+    
+    # calculate garuds statistic
+    gStat = np.sum(freqs[:g])**2 + np.sum(freqs[g:]**2)
+    
+    return(gStat)
+
+
+def garudsStat(stat, geno, pos, cut_height=None, metric='euclidean', window_size=1200, step_size=600):
+    
+    """
+    Calculates G12/G123/H12
+    """
+        
+    # Do we want to cluster the Multi-locus genotypes (MLGs), or just group MLGs if they are identical
+    if stat == "G12":
+        garudsStat = allel.moving_statistic(geno, clusterMultiLocusGenotypes, size=window_size, step=step_size, metric=metric, cut_height=cut_height, g=2)
+    elif stat == "G123":
+        garudsStat = allel.moving_statistic(geno, clusterMultiLocusGenotypes, size=window_size, step=step_size, metric=metric, cut_height=cut_height, g=3)
+    elif stat == "H12":
+        garudsStat,_,_,_ = allel.moving_garud_h(geno, size=window_size, step=step_size)
+    else:
+        raise ValueError("Statistic is not G12/G123/H12")
+
+    midpoint = allel.moving_statistic(pos, np.median, size=window_size, step=step_size)
+    
+    return(garudsStat, midpoint)
+
+
+
+#### Load cohort data and their indices in genotype data
+### run garudStat for that query. already loaded chroms 
+
+cohorts = getCohorts(metadata=metadata, 
+                    columns=snakemake.params.columns, 
+                    minPopSize=snakemake.params.minPopSize)
+
+
+# Loop through each cohort, manipulate genotype arrays and calculate chosen Garuds Statistic
+for idx, cohort in cohorts.iterrows():
+
+    if stat in ['H1', 'H12', 'H123']:
+        # get indices for haplotype Array and filter
+        hapInds = np.sort(np.concatenate(((cohort['indices']*2),((cohort['indices']*2)+1))))
+        gt_cohort = haps.take(hapInds, axis=1)
+    elif stat in ['G12', 'G123']:
+        # filter to correct loc, year, species individuals
+        gt_cohort = snps.take(cohort['indices'], axis=1)
+    else:
+        raise ValueError("Statistic is not G12/G123/H1/H12")
+
+    log(f"--------- Running {stat} on {cohort['cohortText']} ----------")
+    log("filter to biallelic segregating sites")
+
+    ac_cohort = gt_cohort.count_alleles(max_allele=3).compute()
+    # N.B., if going to use to_n_alt later, need to make sure sites are 
+    # biallelic and one of the alleles is the reference allele
+    ref_ac = ac_cohort[:, 0]
+    loc_sites = ac_cohort.is_biallelic() & (ref_ac > 0)
+    gt_seg = da.compress(loc_sites, gt_cohort, axis=0)
+    pos_seg = da.compress(loc_sites, pos, axis=0)
+
+    log(f"compute input data for {stat}")
+    pos_seg = pos_seg.compute()
+
+    if stat in ['G12', 'G123']:
+        gt_seg = allel.GenotypeDaskArray(gt_seg).to_n_alt().compute()
+
+    # calculate G12/G123/H12 and plot figs 
+    gStat, midpoint = garudsStat(stat=stat,
+                                geno=gt_seg, 
+                                pos=pos_seg, 
+                                cut_height=cutHeight,
+                                metric='euclidean',
+                                window_size=windowSize,
+                                step_size=windowStep, 
+                                cluster=True)
+
+    saveAndPlot(statName=stat, 
+                name = cohort['cohortText'],
+                nospacename = cohort['cohortNoSpaceText'],
+                values=gStat, 
+                midpoints=midpoint,
+                prefix=f"../results/selection/{stat}", 
+                chrom=chrom,
+                colour=cohort['colour'],
+                ylim=0.5)
---
+++ workflow/scripts/tools.py
@@ -0,0 +1,481 @@
+import sys
+import pandas as pd
+import allel
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+import seaborn as sns
+import zarr
+import dask
+import dask.array as da
+from dask.diagnostics.progress import ProgressBar
+# silence some warnings
+dask.config.set(**{'array.slicing.split_large_chunks': False})
+import bisect
+import hashlib
+# quieten dask warnings about large chunks
+import plotly.express as px
+import bokeh.layouts
+import bokeh.io
+import bokeh.palettes
+
+
+def log(*msg):
+    print(*msg, file=sys.stdout)
+    sys.stdout.flush()
+
+    
+def getCohorts(metadata, columns=['species_gambiae_coluzzii', 'location'], minPopSize=15):
+    
+    # subset metadata dataFrame and find combinations of variables with more than minPopSize individuals
+    cohorts = metadata[columns]
+    cohorts = cohorts.groupby(columns).size().reset_index().rename(columns={0:'size'})
+    cohorts = cohorts[cohorts['size'] > minPopSize][columns]
+    
+    idxs = []
+    for _, row in cohorts.iterrows():   
+        # create the pandas metadata query for each cohort
+        mycohortQuery = " & ".join([col + " == " + "'" + row.astype(str)[col] + "'" for col in cohorts.columns])
+        # get indices of individuals in each cohort
+        idxs.append(metadata.query(mycohortQuery).index.tolist())
+    
+    cohorts['indices'] = idxs
+    cohorts['cohortText'] = cohorts[columns].agg(' | '.join, axis=1)
+    cohorts['cohortNoSpaceText'] = cohorts['cohortText'].str.replace("|", ".", regex=False).str.replace(" ", "",regex=False)
+    colours =get_colour_dict(cohorts['species_gambiae_coluzzii'], palette="Set1")
+    cohorts['colour'] = cohorts['species_gambiae_coluzzii'].map(colours)
+    return(cohorts.reset_index(drop=True))
+
+
+
+def loadZarrArrays(genotypePath, positionsPath, siteFilterPath, haplotypes=True):
+        
+    """
+    This function reads genotype arrays and applys provided site filter 
+    """
+    
+    if haplotypes==True:
+        snps = zarr.open_array(haplotypePath, mode='r')
+        snps = allel.GenotypeDaskArray(snps).to_haplotypes()
+    else:
+        snps = zarr.open_array(genotypePath, mode = 'r')
+        snps = allel.GenotypeDaskArray(snps)
+    positions = zarr.open_array(positionsPath, mode='r')
+
+    if siteFilterPath is not None:
+        filters = zarr.open(siteFilterPath, mode="r")
+        positions = positions[:][filters[:]]    
+        snps = snps.compress(filters, axis=0)
+        
+    return(snps, positions)
+
+
+def saveAndPlot(statName, cohortText, cohortNoSpaceText, values, midpoints, prefix, chrom, ylim, colour, save=True):
+
+    """
+    Saves to .tsv and plots windowed statistics
+    """
+    
+    assert midpoints.shape == values.shape, f"arrays not same shape, midpoints shape - {midpoints.shape}, value shape - {values.shape}"
+    
+    if save:
+        # store windowed statistics as .tsv 
+        df = pd.DataFrame({'midpoint':midpoints, statName:values})
+        df.to_csv(f"{prefix}/{statName}_{cohortNoSpaceText}.{chrom}.tsv", sep="\t", index=False)
+
+    xtick = np.arange(0, midpoints.max(), 2000000)
+    ylim = np.max([ylim, values.max()])
+    plt.figure(figsize=[20,10])
+    sns.lineplot(midpoints, values, color=colour)
+    plt.xlim(0, midpoints.max()+1000)
+    plt.ylim(0, ylim)
+    plt.yticks(fontsize=14)
+    plt.xticks(xtick, rotation=45, ha='right', fontsize=14)
+    plt.ticklabel_format(style='plain', axis='x')
+    plt.title(f"{statName} | {cohortText} | Chromosome {chrom}", fontdict={'fontsize':20})
+    if save: plt.savefig(f"{prefix}/{statName}_{cohortNoSpaceText}.{chrom}.png",format="png")
+    plt.show()
+    
+
+    
+
+inversionDict = {"2La" : ("2L", "karyotype/2La_targets.txt"),
+                 "2Rj" : ("2R", "karyotype/2Rj_targets.txt"),
+                 "2Rb" : ("2R", "karyotype/2Rb_targets.txt"),
+                 "2Rc_col" : ("2R", "karyotype/2Rc_col_targets.txt"),
+                 "2Rc_gam" : ("2R", "karyotype/2Rc_gam_targets.txt"),
+                 "2Rd" : ("2R", "karyotype/2Rd_targets.txt"),
+                 "2Ru" : ("2R", "karyotype/2Ru_targets.txt")}
+
+
+def import_inversion(inversion):
+
+    '''Load the tag SNPs for the appropriate inversion.'''
+
+    path = inversionDict[inversion][1]
+    targets = pd.read_csv(path, header=None).rename(columns={0:'pos'})    
+    return targets
+
+def calculate_genotype_at_concordant_sites(callset, targets):
+
+    '''Calculate the average number of alternate alleles for each specimen at
+    each tag SNP.'''
+
+    bool_ =  np.isin(callset['pos'], targets['pos'])
+    genos = callset["geno"].compress(bool_, axis=0)
+    alt_count = genos.to_n_alt()
+    is_called = genos.is_called()
+
+    av_gts = np.mean(np.ma.MaskedArray(alt_count,
+                                       mask=~is_called), axis=0).data
+    match_dict = {0: None, 1: None, 2: None}
+
+    for value in [0, 1, 2]:
+        n_matches = np.sum(np.ma.MaskedArray(alt_count,
+                                             mask=~is_called) == value,
+                            axis=0).data
+        match_dict[value] = n_matches
+
+    total_sites = np.sum(is_called, axis=0)
+
+    return av_gts, total_sites, match_dict[0], match_dict[1], match_dict[2]
+    
+
+def compkaryo(callset, inversion):
+
+    '''Extract tag SNPs and desired specimens and calculate the average
+    number of alternate alleles.'''
+
+    targets = import_inversion(inversion)
+
+    av_gts, total_sites, num_0, num_1, num_2 = calculate_genotype_at_concordant_sites(callset, targets)
+
+    return av_gts, total_sites, num_0, num_1, num_2
+   
+
+def get_colour_dict(populations, palette="Set1"):
+    """
+    This function creates a colour palette for a provided list, returning a dict
+    """
+
+    cmap = plt.get_cmap(palette, len(np.unique(populations)))    # PiYG
+    colors = []
+    for i in range(cmap.N):
+        rgb = cmap(i)[:3] # will return rgba, we take only first 3 so we get rgb
+        colors.append(matplotlib.colors.rgb2hex(rgb))
+    pop_colours = {A: B for A, B in zip(np.unique(populations), colors)}
+    return(pop_colours)
+
+
+def legend_without_duplicate_labels(ax, **kwargs):
+    handles, labels = ax.get_legend_handles_labels()
+    unique = [(h, l) for i, (h, l) in enumerate(zip(handles, labels)) if l not in labels[:i]]
+    ax.legend(*zip(*unique), **kwargs)
+
+def plot_pca_coords(coords, model, pc1, pc2, ax, sample_population, samples, pop_colours):
+        sns.despine(ax=ax, offset=5)
+        x = coords[:, pc1]
+        y = coords[:, pc2]
+        for pop in sample_population:
+            treatment = samples[samples['partner_sample_id'] == pop]['location2'].values[0]
+            flt = (sample_population == pop)
+            ax.plot(x[flt], y[flt], marker='o', linestyle=' ', color=pop_colours[treatment], 
+                    label=treatment, markersize=14, mec='k', mew=.5)
+
+        texts = [plt.text(x[i], y[i], pop, fontsize='small', ha='center', va='center') for i, pop in enumerate(sample_population)]
+        adjust_text(texts)
+
+        ax.set_xlabel('PC%s (%.1f%%)' % (pc1+1, model.explained_variance_ratio_[pc1]*100))
+        ax.set_ylabel('PC%s (%.1f%%)' % (pc2+1, model.explained_variance_ratio_[pc2]*100))
+
+def fig_pca(coords, model, title, path, samples, pop_colours,sample_population=None):
+        if sample_population is None:
+            sample_population = samples['location2'].values
+        # plot coords for PCs 1 vs 2, 3 vs 4
+        fig = plt.figure(figsize=(10, 5))
+        ax = fig.add_subplot(1, 2, 1)
+        plot_pca_coords(coords, model, 0, 1, ax, sample_population, samples, pop_colours)
+        ax = fig.add_subplot(1, 2, 2)
+        plot_pca_coords(coords, model, 2, 3, ax, sample_population, samples, pop_colours)
+        legend_without_duplicate_labels(ax)
+        fig.suptitle(title, y=1.02)
+        
+        fig.savefig(path, bbox_inches='tight', dpi=300)
+
+def pca(geno, chrom, ploidy, dataset, populations, samples, pop_colours, prune=True, scaler=None):
+    if prune is True:
+        if ploidy > 1:
+            geno = geno.to_n_alt()
+        geno = ld_prune(geno, size=500, step=200,threshold=0.2)
+    else:
+        if ploidy > 1:
+            geno = geno.to_n_alt()
+        
+    coords1, model1 = allel.pca(geno, n_components=10, scaler=scaler)
+
+    fig_pca(coords1, model1, f"PCA {chrom} {dataset}", f"gaardian/PCA/PCA-{chrom}-{dataset}", samples, pop_colours, sample_population=populations)
+
+
+
+def ld_prune(gn, size, step, threshold=.1, n_iter=1):
+    """
+    Performs LD pruning, originally from Alistair Miles' blog. 
+    """
+    for i in range(n_iter):
+        loc_unlinked = allel.locate_unlinked(gn, size=size, step=step, threshold=threshold)
+        n = np.count_nonzero(loc_unlinked)
+        n_remove = gn.shape[0] - n
+        print('iteration', i+1, 'retaining', n, 'removing', n_remove, 'variants')
+        gn = gn.compress(loc_unlinked, axis=0)
+    return gn
+
+
+
+
+
+#####  PCA function from alistair ####
+
+def hash_params(*args, **kwargs):
+    """Helper function to hash analysis parameters."""
+    o = {
+        'args': args,
+        'kwargs': kwargs
+    }
+    s = json.dumps(o, sort_keys=True).encode()
+    h = hashlib.md5(s).hexdigest()
+    return h
+
+
+def run_pca(contig,
+            gt,
+            pos,
+            df_samples,
+    region_start=None, 
+    region_stop=None,
+    sample_sets="v3.2",
+    sample_query=None,
+    #site_mask="gamb_colu_arab",
+    site_mask="gamb_colu",
+    min_minor_ac=3,
+    max_an_missing=0,
+    n_snps=100000,
+    snp_offset=0,
+    n_components=10,
+    results_dir=""):
+    """Main function to run a PCA.
+    
+    Parameters
+    ----------
+    contig : str
+        Chromosome arm, e.g., '3L'.
+    region_start : int, optional
+        Start position of contig region to use.
+    region_stop : int, optional
+        Stop position of contig region to use.
+    sample_sets : str or list of str, optional
+        Sample sets to analyse.
+    sample_query : str, optional
+        A pandas query string to select specific samples.
+    site_mask : {'gamb_colu_arab', 'gamb_colu', 'arab'}
+        Which site mask to apply.
+    min_minor_ac : int
+        Minimum minor allele count.
+    max_an_missing : int
+        Maximum number of missing allele calls.
+    n_snps : int
+        Approximate number of SNPs to use.
+    snp_offset : int
+        Offset when thinning SNPs.
+    n_components : int
+        Number of PCA components to retain.
+
+    Returns
+    -------
+    data : pandas DataFrame
+        Data frame with one row per sample, including columns "PC1", "PC2", etc.
+    evr : numpy array
+        Explained variance ratio per principal component.
+    
+    """
+    
+    # construct a key to save the results under
+    results_key = hash_params(
+        contig=contig,
+        region_start=region_start, 
+        region_stop=region_stop,
+        sample_sets=sample_sets,
+        sample_query=sample_query,
+        site_mask=site_mask,
+        min_minor_ac=min_minor_ac,
+        max_an_missing=max_an_missing,
+        n_snps=n_snps,
+        snp_offset=snp_offset,
+        n_components=n_components
+    )
+
+    # define paths for results files
+    data_path = f'{results_dir}/{results_key}-data.csv'
+    evr_path = f'{results_dir}/{results_key}-evr.npy'
+
+    try:
+        # try to load previously generated results
+        data = pd.read_csv(data_path)
+        evr = np.load(evr_path)
+        return data, evr
+    except FileNotFoundError:
+        # no previous results available, need to run analysis
+        print(f'running analysis: {results_key}')
+    
+    print('setting up inputs')
+
+    if region_start or region_stop:
+        # locate region within contig
+        loc_region = slice(
+            bisect.bisect_left(pos, region_start) if region_start else None,
+            bisect.bisect_right(pos, region_stop) if region_stop else None,
+        )
+        gt = gt[loc_region]
+    
+    if sample_query:
+        # locate selected samples
+        loc_samples = df_samples.eval(sample_query).values
+        df_samples = df_samples.loc[loc_samples, :]
+        gt = da.compress(loc_samples, gt, axis=1)
+        
+    print('locating segregating sites within desired frequency range')
+
+    # perform allele count
+    ac = allel.GenotypeDaskArray(gt).count_alleles(max_allele=3).compute()
+    
+    # calculate some convenience variables
+    n_chroms = gt.shape[1] * 2
+    an_called = ac.sum(axis=1)
+    an_missing = n_chroms - an_called
+    min_ref_ac = min_minor_ac
+    max_ref_ac = n_chroms - min_minor_ac
+
+    # here we choose biallelic sites involving the reference allele
+    loc_seg = np.nonzero(ac.is_biallelic() & 
+                         (ac[:, 0] >= min_ref_ac) & 
+                         (ac[:, 0] <= max_ref_ac) & 
+                         (an_missing <= max_an_missing))[0]
+    
+    print('preparing PCA input data')
+
+    # thin SNPs to approximately the desired number
+    snp_step = loc_seg.shape[0] // n_snps
+    loc_seg_ds = loc_seg[snp_offset::snp_step]
+
+    # subset genotypes to selected sites
+    gt_seg = da.take(gt, loc_seg_ds, axis=0)
+    
+    # convert to genotype alt counts
+    gn_seg = allel.GenotypeDaskArray(gt_seg).to_n_alt().compute()
+    
+    # remove any edge-case variants where all genotypes are identical
+    loc_var = np.any(gn_seg != gn_seg[:, 0, np.newaxis], axis=1)
+    gn_var = np.compress(loc_var, gn_seg, axis=0)
+
+    print('running PCA')
+
+    # run the PCA
+    coords, model = allel.pca(gn_var, n_components=n_components)
+    
+    # add PCs to dataframe
+    data = df_samples.copy()
+    for i in range(n_components):
+        data[f'PC{i+1}'] = coords[:, i]
+    
+    # save results
+    evr = model.explained_variance_ratio_
+    data.to_csv(data_path, index=False)
+    np.save(evr_path, evr)
+    print(f'saved results: {results_key}')
+    
+    return data, evr
+    
+
+def plot_variance(evr, **kwargs):
+    """Plot a bar chart showing variance explained by each principal
+    component."""
+    
+    # prepare variables
+    y = evr * 100
+    x = [str(i+1) for i in range(len(y))]
+    
+    # setup plotting options
+    plot_kwargs = dict(
+        labels={
+            'x': 'Principal component',
+            'y': 'Explained variance (%)',
+        },
+        width=600,
+        height=400
+    )
+    # apply any user overrides
+    plot_kwargs.update(kwargs)
+
+    # make a bar plot
+    fig = px.bar(x=x, y=y, **plot_kwargs)
+    fig.show()
+    
+
+def jitter(a, f):
+    r = a.max() - a.min()
+    return a + f * np.random.uniform(-r, r, a.shape)
+
+
+def plot_coords(
+    data,
+    x='PC1',
+    y='PC2',
+    filename='foo',
+    jitter_frac=0.02,
+    random_seed=42,
+    **kwargs,
+    ):
+
+    # setup data
+    data = data.copy()
+    
+    # apply jitter if desired - helps spread out points when tightly clustered
+    if jitter_frac:
+        np.random.seed(random_seed)
+        data[x] = jitter(data[x], jitter_frac)
+        data[y] = jitter(data[y], jitter_frac)
+            
+    # convenience variables
+    data['country_location'] = data['country'] + ' - ' + data['location']
+    data['size'] = 1  # hack to allow us to control marker size
+    
+    # setup plotting options
+    plot_kwargs = dict(
+        width=700,
+        height=500,
+        hover_name='sample_id',
+        hover_data=[
+            'partner_sample_id',
+            #'species', 
+            'country', 
+            'location',
+            # 'insecticide',
+            # 'phenotype',
+            # 'exposure_time',
+            # 'concentration',
+            'year', 
+        ],
+        size='size',
+        size_max=8,
+        opacity=0.9,
+        render_mode='svg',
+    )
+    # apply any user overrides
+    plot_kwargs.update(kwargs)
+
+    # 2D scatter plot
+    fig = px.scatter(data, x=x, y=y, **plot_kwargs)
+    #fig.write_image(f"{filename}.png", format=".png")
+    fig.show()
+
+

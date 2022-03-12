c62f3bd623839297622a42abb5653896611f5397
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2022-03-12 22:31:27 +0000

    f2 haps!

--- workflow/scripts/f2HaplotypeLength.py
+++ workflow/scripts/f2HaplotypeLength.py
@@ -34,71 +34,95 @@ else:
     metadata = pd.read_csv(snakemake.params['metadata'], sep="\t")
 
 
+@njit()
+def scanRight(geno1, geno2, upperBreakpoint, max): 
+    # Reset position for next loop
+    gn1 = geno1[upperBreakpoint]
+    gn2 = geno2[upperBreakpoint]
+
+    # Scan right along genome, as long as two inds are not both homozygous but different
+    while not (gn1[0] == gn1[1]) & (gn2[0] == gn2[1]) & ((gn1 != gn2).all()) & (-1 not in gn1 and -1 not in gn2):
+
+        upperBreakpoint += 1
+        if upperBreakpoint == max-1: # limit the upper breakpoint at end of the contig
+            return(upperBreakpoint)
+
+        gn1 = geno1[upperBreakpoint]
+        gn2 = geno2[upperBreakpoint]
+        
+    return(upperBreakpoint)
+
+@njit()
+def scanLeft(geno1, geno2, lowerBreakpoint): 
+
+    # subset genotypes to this position, because we need somethign to start the while loop?
+    gn1 = geno1[lowerBreakpoint]
+    gn2 = geno2[lowerBreakpoint]
+
+    # Scan left along genome
+    while not (gn1[0] == gn1[1]) & (gn2[0] == gn2[1]) & ((gn1 != gn2).all()) & (-1 not in gn1 and -1 not in gn2):
+
+        lowerBreakpoint -= 1
+        if lowerBreakpoint == 0: # limit lower breakpoint at zero, start of contig
+            return(lowerBreakpoint)
+
+        gn1 = geno1[lowerBreakpoint]
+        gn2 = geno2[lowerBreakpoint]
+        
+    return(lowerBreakpoint)
+
+
+@njit()
+def f2scans(dblton_arr, snps, pos):
+
+    starts = []# np.empty((len(dblton_arr)),dtype='uint8')
+    ends = []# np.empty((len(dblton_arr)),dtype='uint8')
+    dbltonpos = []
+
+    for idx in range(0, len(dblton_arr)):
+
+        geno1 = snps[:, dblton_arr[idx][0]]
+        geno2 = snps[:, dblton_arr[idx][1]]
+        # get boolean of dblton idx
+        #dblton_idx = bisect.bisect_left(pos, dblton_arr[idx][2])
+        dblton_idx = np.searchsorted(pos, dblton_arr[idx][2])
+
+        upperBreakpoint = scanRight(geno1, geno2, dblton_idx, len(pos))
+        # Scan left along genome
+        lowerBreakpoint = scanLeft(geno1, geno2, dblton_idx)
+
+        starts.append(pos[lowerBreakpoint])
+        ends.append(pos[upperBreakpoint])
+        dbltonpos.append(dblton_arr[idx][2])
+    
+    return(np.array(starts), np.array(ends), np.array(dbltonpos))
+
+
+
+########## main #################
+
+
 snps = {}
 pos = {}
 
 # Load Arrays
-snps[contig], pos[contig] = loadZarrArrays(genotypePath=genotypePath, 
+snps, pos = loadZarrArrays(genotypePath=genotypePath, 
                                             positionsPath=positionsPath,
                                             siteFilterPath=None,
                                             cloud=cloud)
-ac = snps[contig].count_alleles()
+ac = snps.count_alleles()
 seg = ac.is_segregating()
-snps[contig] = snps[contig].compress(seg, axis=0).compute(numworkers=12)
-pos[contig] = pos[contig][seg]
+snps = snps.compress(seg, axis=0).compute(numworkers=12)
+pos = pos[seg]
 
 ### Load doubletons
 dblton = pd.read_csv(snakemake.input['f2variantPairs'], sep="\t")
 dblton = dblton.query("contig == @contig")
+dblton_arr = dblton[['idx1', 'idx2', 'pos']].to_numpy()
 
-with open(f"results/f2HapLengths_{contig}.tsv", mode='w') as file:
-    file.close()
+# extract np array 
+snps = snps.values
+# Run F2 hap length scans
+starts, ends, dbltonpos = f2scans(dblton_arr, snps, pos)
 
-for idx, row in dblton.iterrows():
 
-    # take one doubleton at a time, get contig, pos, individual1 and 2
-    contig, dbltonpos, idx1, idx2 = row[['contig', 'pos', 'idx1', 'idx2']]
-    ## subset to individuals, we need whole chrom atm
-
-    # subset genotypes to these individuals
-    geno = snps[contig].take([idx1,idx2], axis=1)
-    # get boolean of dblton idx
-    dblton_idx = np.where(pos[contig] == dbltonpos)[0]
-    # subset genotypes to this position, because we need somethign to start the while loop?
-    gn1 = geno[dblton_idx, 0]
-    gn2 = geno[dblton_idx, 1]
-
-    # we will add to and subtract from the dblton index 
-    upperBreakpoint = dblton_idx.copy()
-    lowerBreakpoint = dblton_idx.copy()
-
-    # Scan right along genome, as long as two inds are not both homozygous but different
-    while not (gn1.is_hom() & gn2.is_hom() & (gn1 != gn2)).all():
-
-        upperBreakpoint += 1
-        if upperBreakpoint == pos[contig].shape[0]: # limit the upper breakpoint at end of the contig
-            break
-
-        gn1 = geno[upperBreakpoint, 0]
-        gn2 = geno[upperBreakpoint, 1]
-
-    # Reset position for next loop
-    gn1 = geno[dblton_idx, 0]
-    gn2 = geno[dblton_idx, 1]
-
-    # Scan left along genome
-    while not (gn1.is_hom() & gn2.is_hom() & (gn1 != gn2)).all():
-
-        lowerBreakpoint -= 1
-        if lowerBreakpoint == 0: # limit lower breakpoint at zero, start of contig
-            break
-
-        gn1 = geno[lowerBreakpoint, 0]
-        gn2 = geno[lowerBreakpoint, 1]
-
-    start = pos[contig][lowerBreakpoint]
-    end = pos[contig][upperBreakpoint]
-    size = end-start
-
-    with open(f"results/f2HapLengths_{contig}.tsv", mode='a') as file:
-        file.write(f"{contig}\t{str(dbltonpos)}\t{str(size[0])}\t{str(start[0])}\t{str(end[0])}\n")
